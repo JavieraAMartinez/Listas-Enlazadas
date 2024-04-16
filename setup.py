1a5cede9045770a92d8066bde52ff136e54d2ede
Javier Martinez <149551446+JavieraAMartinez@users.noreply.github.com>
2024-04-15 21:42:50 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyLinkedList',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple linked list implementation in Python',
+    author='Tu Nombre',
+    author_email='tu@email.com',
+    license='MIT',
+)
